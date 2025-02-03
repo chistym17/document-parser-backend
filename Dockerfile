0d630940cde20ec152a757dec066e618b4670251
chistym17 <mchisty31@gmail.com>
2025-02-03 23:39:42 +0600

    updated code and simplified

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,3 @@
-FROM python:3.12.3-slim as builder
-
-RUN apt-get update && apt-get install -y \
-    tesseract-ocr \
-    gcc \
-    python3-dev \
-    && rm -rf /var/lib/apt/lists/*
-
-WORKDIR /app
-
-COPY requirements.prod.txt requirements.txt
-
-RUN pip install --no-cache-dir -r requirements.txt
-
-RUN python -m spacy download en_core_web_sm
-
-COPY . .
-
 FROM python:3.12.3-slim
 
 RUN apt-get update && apt-get install -y \
@@ -24,8 +6,10 @@ RUN apt-get update && apt-get install -y \
 
 WORKDIR /app
 
-COPY --from=builder /app /app
-COPY --from=builder /usr/local/lib/python3.12/site-packages /usr/local/lib/python3.12/site-packages
+COPY requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY . .
 
 ENV PYTHONUNBUFFERED=1
 
--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pymongo import MongoClient
-from app.services import text_analysis, text_extraction, text_categorization,text_summarize,sentiment_analyzer,extract_keypoints,image_extraction,link_extraction
+from app.services import text_extraction,image_extraction,link_extraction
 
 app = FastAPI()
 
@@ -13,12 +13,7 @@ app.add_middleware(
     allow_headers=["*"],  
 )
 
-app.include_router(text_analysis.router, prefix="/api/analysis", tags=["analysis"])
 app.include_router(text_extraction.router, prefix="/api/extraction", tags=["extraction"])
-app.include_router(text_categorization.router, prefix="/api/categorization", tags=["categorization"])
-app.include_router(text_summarize.router, prefix="/api/summarize", tags=["categorization"])
-app.include_router(sentiment_analyzer.router, prefix="/api/sentiment", tags=["sentiment"])
-app.include_router(extract_keypoints.router, prefix="/api/extraction", tags=["keywords"])
 app.include_router(image_extraction.router, prefix="/api/extraction", tags=["images"])
 app.include_router(link_extraction.router, prefix="/api/extraction", tags=["links"])
 
